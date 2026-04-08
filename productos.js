7366853edda8ec9821a9aab938fe7fea32591973
Jeisson0620 <Jeisson0620@users.noreply.github.com>
2026-04-08 13:53:34 -0600

    primera subida

---
+++ productos.js
@@ -0,0 +1,130 @@
+const catalogo = {
+    desinfectantes: [
+        {
+            grupo: "Clorizide",
+            marca: "Spin",
+            imagenPrincipal: "CLORIZIDE-91.png",
+            variantes: [
+                { 
+                    subtitulo: "Grano", 
+                    imagen: "CLORIZIDE-91.png",
+                    pesos: ["1.25 Kg", "4.5 Kg", "11.5 Kg", "21 Kg"] 
+                },
+                { 
+                    subtitulo: "Tableta", 
+                    imagen: "CLORIZIDE-91.png",
+                    pesos: ["1.8 Kg", "4 Kg", "11Kg"] 
+                }
+            ]
+        },
+        {
+            grupo: "Trizide",
+            marca: "Spin",
+            imagenPrincipal: "TRIZIDE.png",
+            variantes: [
+                { 
+                    subtitulo: "Trizide 65% Grano", 
+                    imagen: "img/descarga.webp",
+                    pesos: ["1.35 Kg", "4.2 Kg", "12 Kg", "20 Kg"] 
+                },
+                { 
+                    subtitulo: "Trizide 65% Tableta", 
+                    imagen: "img/descarga.webp",
+                    pesos: ["1.8 Kg", "4 Kg", "11 Kg"] 
+                }
+            ]
+        },
+         {
+            grupo: "SHOCK CORRRECTIVO",
+            marca: "Spin",
+            imagenPrincipal: "SHOCK-CORRECTIVO.png",
+            variantes: [
+                { 
+                    subtitulo: "Granular", 
+                    imagen: "CLORIZIDE-91.png",
+                    pesos: ["1.2 Kg", "4 Kg", "10 Kg", "18 Kg"] 
+                },
+               
+            ]
+        },
+    ],
+    balanceadores: [
+        {
+            grupo: "ALCALOS",
+            marca: "Spin",
+            imagenPrincipal: "ALCALOS.png",
+            variantes: [
+                { 
+                    subtitulo: "Alcalos", 
+                    imagen: "ALCALOS.png",
+                    pesos: ["5 Kg", "13 Kg"] 
+                }
+            ]
+        },
+        {
+            grupo: "Alkalin",
+            marca: "Spin",
+            imagenPrincipal: "ALKALIN.png",
+            variantes: [
+                { 
+                    subtitulo: "Alkalin Grano", 
+                    imagen: "ALKALIN.png",
+                    pesos: ["1.3 Kg", "5 Kg", "13 Kg"] 
+                }
+            ]
+        },
+        {
+            grupo: "Acidet",
+            marca: "Spin",
+            imagenPrincipal: "ACIDET.png",
+            variantes: [
+                { 
+                    subtitulo: "Acidet Grano", 
+                    imagen: "ACIDET.png",
+                    pesos: ["1.9 Kg","6.5 Kg", "16 Kg"] 
+                }
+            ]
+        },
+        {
+            grupo: "Cristalin Platinum",
+            marca: "Spin",
+            imagenPrincipal: "CRISTALIN-PLATINUM.png",
+            variantes: [
+                { 
+                    subtitulo: "Cristalin Platinum", 
+                    imagen: "CRISTALIN-PLATINUM.png",
+                    pesos: ["1 L", "5 L", "10 L"] 
+                }
+            ]
+        },
+         {
+            grupo: "Cristalin Green",
+            marca: "Spin",
+            imagenPrincipal: "CRISTALIN-GREEN.png",
+            variantes: [
+                { 
+                    subtitulo: "Cristalin Green", 
+                    imagen: "CRISTALIN-GREEN.png",
+                    pesos: ["1 L"] 
+                }
+            ]
+        },
+         {
+            grupo: "Clarit Plus",
+            marca: "Spin",
+            imagenPrincipal: "CLARIT-PLUS.png",
+            variantes: [
+                { 
+                    subtitulo: "Clarit Plus", 
+                    imagen: "CLARIT-PLUS.png",
+                    pesos: ["1 L", "5 L", "10 L"] 
+                }
+            ]
+        },
+        
+    ],
+
+    clarificantes: [
+        // Aquí agregarías Alcalos, pH Plus, etc.
+    ]
+};
